43ea99c8e68cebafc42cf8e0fa5dece46af6bc4d
Argen <argen.telhaj@outlook.com>
2024-01-22 19:37:22 +0100

    first commit

--- src/main/java/RandomNumberGenerator/Application.java
+++ src/main/java/RandomNumberGenerator/Application.java
@@ -1,13 +1,18 @@
 package RandomNumberGenerator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
+    private static final Logger logger = LoggerFactory.getLogger(YourClass.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+		logger.info("Info level log message");
+        logger.error("Error level log message");
 	}
 
 }
